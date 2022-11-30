09c59830ed39dd1ab4b4e58f3d533195755831f0
Potato <118915670+potato-chan@users.noreply.github.com>
2022-11-30 17:12:25 +0100

    Name of test is corrected

--- test_product_page.py
+++ test_product_page.py
@@ -64,7 +64,7 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
 
 
 @pytest.mark.need_review
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
