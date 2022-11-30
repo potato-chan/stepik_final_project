6cb58bb78458a188b9be291cc00195dbb62fd43c
Potato <dearcreamdeer@gmail.com>
2022-11-30 06:00:08 +0100

    test_main_page.py file added

---
+++ test_main_page.py
@@ -0,0 +1,33 @@
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+from selenium import webdriver
+import pytest
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        page = BasketPage(browser, browser.current_url)
+        page.should_not_be_products_in_the_basket()
+        page.text_empty_basket_is_present()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
